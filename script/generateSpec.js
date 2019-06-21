d4b27a6cdb71f4db06829fa9710a661a718ba3ac
dr-js <dr@dr.run>
2019-06-21 11:36:59 +0800

    dr-server@0.1.0
    notable change:
    - break: use `DR_BROWSER_SCRIPT_TAG` from `dr-js/module/node/resource` directly
    - add `responderCommonExtend()` for quick request payload
    - add: serverFetch
    - allow skip invalid or un-exist pid
    - do not mix `module` import with `bin` import
    - package update

--- script/generateSpec.js
+++ script/generateSpec.js
@@ -1,11 +1,9 @@
 import { resolve } from 'path'
 import { writeFileSync } from 'fs'
 
-import { indentLine } from 'dr-js/module/common/string'
-
 import { collectSourceRouteMap } from 'dr-dev/module/node/export/parse'
 import { generateExportInfo } from 'dr-dev/module/node/export/generate'
-import { autoAppendMarkdownHeaderLink, renderMarkdownFileLink, renderMarkdownExportPath } from 'dr-dev/module/node/export/renderMarkdown'
+import { getMarkdownFileLink, renderMarkdownAutoAppendHeaderLink, renderMarkdownBlockQuote, renderMarkdownExportPath } from 'dr-dev/module/node/export/renderMarkdown'
 import { runMain } from 'dr-dev/module/main'
 
 import { formatUsage } from 'source-bin/option'
@@ -13,30 +11,22 @@ import { formatUsage } from 'source-bin/option'
 const PATH_ROOT = resolve(__dirname, '..')
 const fromRoot = (...args) => resolve(PATH_ROOT, ...args)
 
-const renderMarkdownBinOptionFormat = () => [
-  renderMarkdownFileLink('source-bin/option.js'),
-  '> ```',
-  indentLine(formatUsage(), '> '),
-  '> ```'
-]
-
 runMain(async (logger) => {
-  logger.padLog(`collect sourceRouteMap`)
+  logger.padLog(`generate exportInfoMap`)
   const sourceRouteMap = await collectSourceRouteMap({ pathRootList: [ fromRoot('source') ], logger })
-
-  logger.padLog(`generate exportInfo`)
   const exportInfoMap = generateExportInfo({ sourceRouteMap })
 
   logger.log(`output: SPEC.md`)
   writeFileSync(fromRoot('SPEC.md'), [
     '# Specification',
     '',
-    ...autoAppendMarkdownHeaderLink(
+    ...renderMarkdownAutoAppendHeaderLink(
       '#### Export Path',
       ...renderMarkdownExportPath({ exportInfoMap, rootPath: PATH_ROOT }),
       '',
       '#### Bin Option Format',
-      ...renderMarkdownBinOptionFormat()
+      getMarkdownFileLink('source-bin/option.js'),
+      ...renderMarkdownBlockQuote(formatUsage())
     ),
     ''
   ].join('\n'))
